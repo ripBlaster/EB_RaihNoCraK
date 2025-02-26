49c5f85e7de2385da55fa0ede6209e1bfd2b8723
ripBlaster <149702127+ripBlaster@users.noreply.github.com>
2025-02-26 17:36:47 -0300

    Create permissoes.lua

---
+++ permissoes.lua
@@ -0,0 +1,8 @@
+{
+  "Banned_Players": ["jogador1", "jogador2"],
+  "Crash_Players": ["hacker1", "hacker2"],
+  "Punished_Players": ["trapaceiro1", "trapaceiro2"],
+  "Safe_Chat_Players": ["criança1", "criança2"],
+  "ID_Banned_Players": ["12345678", "87654321"],
+  "People_Who_Can_Ban_Other_People": ["AdminMaster"]
+}
